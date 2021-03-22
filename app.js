17903d693fd20b4c0d147d220c5bf715a2e21880
Alfonso Sarmiento <alfitero6@gmail.com>
2021-03-22 18:04:29 -0300

    Aplicación hecha de consola hecha en Node

---
+++ app.js
@@ -0,0 +1,85 @@
+require('colors');
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const{ inquirerMenu,
+        pausa,
+        leerInput,
+        listadoTareasBorrar,
+        confirmar,
+        mostrarListadoChecklist
+} = require('./helpers/inquirer'); 
+
+const Tareas = require('./models/tareas');
+
+
+const main = async() => {    
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if (tareasDB) {
+        //Establecer tareas
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+
+    do{
+        // Imprimir el menú
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                //Crear opción
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea( desc );
+                guardarDB(tareas.listadoArr);
+            break;
+
+            case '2':
+                tareas.listadoCompleto();
+            break;
+
+            case '3':
+                tareas.listarCompletadas()
+            break;
+
+            case '4':
+                tareas.listarPendientes();
+            break;
+
+            case '5':
+                const ids = await mostrarListadoChecklist( tareas.listadoArr );
+                tareas.toggleCompletadas(ids);
+            break;
+
+            case '6':
+
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '0') {
+                    const ok = await confirmar('¿Está seguro?');
+                    if (ok) {
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada');
+                    }
+                }
+                
+            break;
+        }
+        
+
+        await pausa();
+        
+    }while (opt !== '0');
+
+ 
+}
+
+
+
+main();
+
+
+
+
+
